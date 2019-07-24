51e237deab51dca6385b291cadba302cb4f20d36
Jacob King <jacob@digi.me>
2019-07-24 16:45:35 +0100

    Created client config model.

---
+++ sdk/src/main/java/me/digi/sdk/entities/DMEClientConfiguration.kt
@@ -0,0 +1,20 @@
+package me.digi.sdk.entities
+
+data class DMEClientConfiguration (
+
+    var appId: String,
+    var contractId: String,
+    var privateKeyHex: String
+
+) {
+
+    var globalTimeout: Int = 25
+    var retryOnFail: Boolean = true
+    var retryDelay: Int = 750
+    var retryWithExponentialBackOff: Boolean = true
+    var maxRetryCount: Int = 5
+    var maxConcurrentRequests: Int = 5
+    var debugLogEnabled: Boolean = false
+    var baseUrl: String = "https://api.digi.me/"
+
+}
