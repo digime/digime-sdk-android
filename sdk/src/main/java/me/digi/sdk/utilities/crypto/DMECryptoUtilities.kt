8e6454402bcfbb9a4fc459c128902e7dc66c1d96
Jacob King <jacob@digi.me>
2019-08-07 11:24:31 +0100

    WIP SDK in progress

--- sdk/src/main/java/me/digi/sdk/utilities/crypto/DMECryptoUtilities.kt
+++ sdk/src/main/java/me/digi/sdk/utilities/crypto/DMECryptoUtilities.kt
@@ -1,24 +1,30 @@
 package me.digi.sdk.utilities.crypto
 
+import android.content.Context
+import me.digi.sdk.legacy.crypto.PKCS12Utils
 import org.spongycastle.jce.provider.BouncyCastleProvider
 import java.io.FileInputStream
 import java.security.KeyStore
 import java.security.Security
 
-object DMECryptoUtilities {
+class DMECryptoUtilities(val context: Context) {
 
-    private const val keyCAPrivateKey = "Consent Access Contract"
+    companion object {
+        private const val keyCAPrivateKey = "Consent Access Contract"
+    }
+
     private val keyStore by lazy {
         Security.addProvider(BouncyCastleProvider())
         KeyStore.getInstance("pkcs12", "SC")
     }
 
-
-
     fun privateKeyHexFrom(p12File: String, password: String): String {
 
-        val dataStream = FileInputStream(p12File)
-        keyStore.load(dataStream, password.toCharArray())
+        val list = context.assets.list("")
+
+        val inStream = context.assets.open("CA_RSA_PRIVATE_KEY.p12")
+        PKCS12Utils.getKeysFromP12Stream(inStream, "monkey periscope")
+//        keyStore.load(dataStream, password.toCharArray())
 
         val privateKey = keyStore.getKey(keyCAPrivateKey, password.toCharArray())
 
--- sdk/src/test/java/me/digi/sdk/crypto/DMECryptoUtilitiesSpec.kt
+++ sdk/src/test/java/me/digi/sdk/crypto/DMECryptoUtilitiesSpec.kt
@@ -2,14 +2,22 @@ package me.digi.sdk.crypto
 
 import me.digi.sdk.utilities.crypto.DMECryptoUtilities
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
 
+@Config(assets="")
+@RunWith(RobolectricTestRunner::class)
 class DMECryptoUtilitiesSpec {
 
     @Test
     fun `given valid p12 file and pass, key is imported`() {
-        ClassLoader.getSystemClassLoader().getResource("src/test/assets/TestPrivateKey.p12")
-        val p12Pass = "digime"
-        val key = DMECryptoUtilities.privateKeyHexFrom("src/test/assets/TestPrivateKey.p12", p12Pass)
+        val p12Pass = "monkey periscope"
+
+        val ctx = RuntimeEnvironment.systemContext
+
+        val key = DMECryptoUtilities(ctx).privateKeyHexFrom("src/test/assets/CA_RSA_PRIVATE_KEY.p12", p12Pass)
         print(key)
         assert(key != "")
     }
