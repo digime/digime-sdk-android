b62e04a60b1f0d641aac6c2f3a7600e88f283de4
Jacob King <jacob@digi.me>
2019-09-26 16:42:20 +0100

    Added exponential backoff.

--- sdk/src/main/java/me/digi/sdk/api/interceptors/DMERetryInterceptor.kt
+++ sdk/src/main/java/me/digi/sdk/api/interceptors/DMERetryInterceptor.kt
@@ -22,8 +22,19 @@ class DMERetryInterceptor(private val config: DMEClientConfiguration): Intercept
         while (!response.isSuccessful && retryAttemptsRemaining > 0) {
 
             retryAttemptsRemaining--
+
+            val waitTime = {
+                var t = config.retryDelay.toLong()
+
+                if (config.retryWithExponentialBackOff) {
+                    repeat(config.maxRetryCount - retryAttemptsRemaining) { t*=2 }
+                }
+
+                t
+            }.invoke()
+            
             // TODO: Implement error based filtering to avoid non-recoverable retries.
-            waitMillis(config.retryDelay.toLong())
+            waitMillis(waitTime)
             response = chain.proceed(request)
 
         }
