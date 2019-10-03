532873e12c49fb2a43ebdd7bf69518c4fe7b9760
Jacob King <jacob@digi.me>
2019-10-03 16:36:57 +0100

    Tidied up gradle and modules.

--- sdk/src/main/java/me/digi/sdk/entities/DMESDKAgent.kt
+++ sdk/src/main/java/me/digi/sdk/entities/DMESDKAgent.kt
@@ -1,11 +1,13 @@
 package me.digi.sdk.entities
 
+import me.digi.sdk.BuildConfig
+
 data class DMESDKAgent (
 
     @JvmField
     val name: String = "android",
 
     @JvmField
-    val version: String = "1.6.0"
+    val version: String = BuildConfig.VERSION_NAME
 
 )
