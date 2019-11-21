4ee36fa99cf170779a4128e1134d860b1bde1ee0
Jacob King <jacob@digi.me>
2019-11-21 09:48:53 +0000

    Fixed layout of consent selection window.

--- sdk/src/main/java/me/digi/sdk/ui/ConsentModeSelectionDialogue.kt
+++ sdk/src/main/java/me/digi/sdk/ui/ConsentModeSelectionDialogue.kt
@@ -1,16 +1,20 @@
 package me.digi.sdk.ui
 
 import android.app.DialogFragment
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import me.digi.sdk.R
 import kotlinx.android.synthetic.main.consent_mode_selection_fragment.*
 
 class ConsentModeSelectionDialogue: DialogFragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        dialog.apply {
+            requestWindowFeature(Window.FEATURE_NO_TITLE)
+            setCancelable(false)
+        }
         return inflater?.inflate(R.layout.consent_mode_selection_fragment, container, false)
     }
 
@@ -20,5 +24,14 @@ class ConsentModeSelectionDialogue: DialogFragment() {
         consentSelectionHeader.text = getText(R.string.consent_mode_selection_heading)
         consentSelectionDetail.text = getText(R.string.consent_mode_selection_detail)
         consentSelectionShareAsGuest.text = getText(R.string.consent_mode_selection_share_as_guest)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.apply {
+            setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT)
+            setDimAmount(0.8F)
+            setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        }
     }
 }
