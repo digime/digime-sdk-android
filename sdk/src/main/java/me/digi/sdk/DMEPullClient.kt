d45a62411bb1be8f19cc3ce0f6663b64e7408cb0
Melisa69 <melisa@digi.me>
2020-04-21 16:56:08 +0200

    Fix internal server error

--- sdk/src/main/java/me/digi/sdk/DMEPullClient.kt
+++ sdk/src/main/java/me/digi/sdk/DMEPullClient.kt
@@ -242,7 +242,7 @@ class DMEPullClient(val context: Context, val configuration: DMEPullConfiguratio
 
                 // If an error is encountered from this call, we inspect it to see if it's an
                 // 'InvalidToken' error, meaning that the ACCESS token has expired.
-                if (error is DMEAPIError.INVALID_REFRESH_TOKEN && error.code == "InvalidToken") {
+                if (error is DMEAPIError && (error.code == "InvalidToken" || error.code == "InternalServerError")) {
 
                     // If so, we take the active session and expired credentials and try to refresh them.
                     Single.just(Pair(nativeConsentManager.sessionManager.currentSession!!, activeCredentials!!))
@@ -252,7 +252,7 @@ class DMEPullClient(val context: Context, val configuration: DMEPullConfiguratio
 
                             // If an error is encountered from this call, we inspect it to see if it's an
                             // 'InvalidToken' error, meaning that the REFRESH token has expired.
-                            if (error is DMEAPIError.INVALID_REFRESH_TOKEN && error.code == "InvalidToken") {
+                            if (error is DMEAPIError && (error.code == "InvalidToken" || error.code == "InternalServerError")) {
 
                                 // If so, we need to obtain a new set of credentials from the digi.me
                                 // application. Process the flow as before, for ongoing acces, provided
