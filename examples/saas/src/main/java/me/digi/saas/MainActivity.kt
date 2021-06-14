9035302cffe67633b37b4f7a9ea3e1e20b18aab5
karickenan <kenan@digi.me>
2021-06-14 13:14:10 +0200

    Crated Saas example app init

---
+++ examples/saas/src/main/java/me/digi/saas/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.digi.saas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
