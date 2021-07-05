ed24ade3841e6d15d1cba11ea3d54f48daae7837
karickenan <kenan@digi.me>
2021-07-05 16:26:37 +0200

    minor refactor

--- examples/saas/src/main/java/me/digi/saas/MainActivity.kt
+++ examples/saas/src/main/java/me/digi/saas/MainActivity.kt
@@ -2,44 +2,16 @@ package me.digi.saas
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import me.digi.sdk.interapp.DMEAppCommunicator
 import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var navController: NavController
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setUpNavigation()
-    }
-
-    private fun setUpNavigation() {
-        val navHostFragment = supportFragmentManager.findFragmentById(
-            R.id.nav_host_container
-        ) as NavHostFragment
-        navController = navHostFragment.navController
-
-        // Setup the bottom navigation view with navController
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
-        bottomNavigationView.setupWithNavController(navController)
-
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            when (destination.id) {
-                R.id.authFragment,
-                R.id.onboardFragment ->
-                    bottomNavigationView.visibility = View.GONE
-                else -> bottomNavigationView.visibility = View.VISIBLE
-            }
-        }
     }
 
     override fun onResume() {
